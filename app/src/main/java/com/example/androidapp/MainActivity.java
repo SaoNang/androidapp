d42c6e548ce98d06e10ad7c2b510bd2cba668f16
Sao Nang <sainangreall99@gmail.com>
2020-11-22 17:50:07 +0700

    nangapp

---
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onCreateOptionsMenu(ContextMenu menu, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+    }
+
+}
